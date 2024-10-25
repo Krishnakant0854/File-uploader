4f9e78fb56f7f5ab5d1b1a7762fad63165050517
Krishnakant0854 <165988710+Krishnakant0854@users.noreply.github.com>
2024-10-25 14:59:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded());
 
